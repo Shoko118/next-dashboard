991d3ce9878c2c864b6a1af5da79de3cf1096b1f
Bryan Liaw <bryan.liaw@involve.asia>
2023-12-22 14:48:01 +0800

    :package: Ui package for Floating Input

--- app/design-system/page.tsx
+++ app/design-system/page.tsx
@@ -1,7 +1,10 @@
+import FloatingInput from '../_packages/FloatingInput';
+
 export default function DesignSystem() {
   return (
     <div>
       <div>Design System Page</div>
+      <FloatingInput />
     </div>
   );
 }
